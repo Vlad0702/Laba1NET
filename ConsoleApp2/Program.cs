e255f2c3fba033ee637894ec88f6e32b71c190db
vlad07072002li@gmail.com <vlad07072002li@gmail.com>
2020-03-11 20:41:55 +0200

    Для пробы

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaNET1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            int li = Convert.ToInt32(Math.Pow(age, 2));
+            Console.WriteLine("Число в квадрате = ");
+            Console.WriteLine(li);
+            Console.ReadKey();
+
+        }
+    }
+}
